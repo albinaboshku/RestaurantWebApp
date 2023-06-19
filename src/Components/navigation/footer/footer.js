e19d0eaf91829b70785712e9b3d8418637d56f6d
albinaboshku <albinaboshku@gmail.com>
2023-06-19 17:29:48 +0200

    feat: making changes on ui

--- src/Components/navigation/footer/footer.js
+++ src/Components/navigation/footer/footer.js
@@ -1,40 +1,49 @@
-import React from 'react';
-import './footer.css';
-import { Container, Col, Row } from 'reactstrap';
-import fb from '../../../Assests/icons/footer/fb.png';
-import insta from '../../../Assests/icons/footer/insta.png';
+import React from "react";
+import "./footer.css";
+import { Container, Col, Row } from "reactstrap";
+import fb from "../../../Assests/icons/footer/fb.png";
+import insta from "../../../Assests/icons/footer/insta.png";
 
 const footer = () => {
-    return (
-        <footer id="footer" className="footer">
-            <Container>
-                <Row>
-                    <Col>
-                        <div className="storage">
-                            <a href='#footer'>About Us</a>
-                            
-                            <a href="#map">Map</a>
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className="storage2">
-                            <a href="https://www.instagram.com/bambo_pizza/" ><img src={insta} alt="Icon" /></a>
-                            <a href="https://www.facebook.com/Bambo-Pizza-Bar-280897071947445/" ><img src={fb} alt="Icon" /></a>
-                        </div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <p className="footer-text">Contact: 069 48 84 550<br /> Oraret e disponueshmërisë: Çdo ditë 10:00-22:30 <br/>Adresa: Rruga e Kavajës, Kompleksi "Delijorgji" Tiranë <br /> Order: 069 48 84 550, 0424 22 222 <br /> 
-                            &copy; COPYRIGHT 2021 reserved by Albina Boshku<br /><br/>
-                            
-                              </p>
-                    </Col>
-                </Row>
-            </Container>
-            
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer id="footer" className="footer">
+      <Container>
+        <Row>
+          <Col>
+            <div className="storage">
+              <a href="#footer">About Us</a>
 
-        </footer>
-    );
-}
+              <a href="#map">Map</a>
+            </div>
+          </Col>
+          <Col>
+            <div className="storage2">
+              <a href="https://www.instagram.com/bambo_pizza/">
+                <img src={insta} alt="Icon" />
+              </a>
+              <a href="https://www.facebook.com/Bambo-Pizza-Bar-280897071947445/">
+                <img src={fb} alt="Icon" />
+              </a>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <p className="footer-text">
+              Nr. kontakti : 069 4884 550
+              <br /> Oraret e disponueshmërisë: Çdo ditë 10:00-22:30 <br />
+              Adresa: Rruga e Kavajës, Kompleksi "Delijorgji", Tiranë <br />{" "}
+              Porosit në: 069 4884 550, 0424 22 222 <br />
+              &copy;
+              {currentYear} Albina Boshku.
+              <br />
+              <br />
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 export default footer;
--- src/Containers/Home/Home.js
+++ src/Containers/Home/Home.js
@@ -1,30 +1,24 @@
-import React,{Component} from 'react';
+import React, { Component } from "react";
 // import './Home.css';
-import Toolbar from '../../Components/navigation/toolbar/toolbar';
-import Header from '../../Components/sections/section-header/section';
+import Toolbar from "../../Components/navigation/toolbar/toolbar";
+import Header from "../../Components/sections/section-header/section";
 
+class Home extends Component {
+  state = {
+    lat: 0,
+    long: 0,
+  };
+  componentWillUpdate() {}
 
-class Home extends Component{
-    state={
-        lat:0,
-        long:0
-    }
-    componentWillUpdate(){
-        
-        
-    }
-    
-    render(){
-        return(
-            <div>
-            <Header data={this.props.data}>
-            <Toolbar count={this.props.count}/>
-            </Header>
-            </div>
-        );
-    }
-   
-
+  render() {
+    return (
+      <div>
+        <Header data={this.props.data}>
+          <Toolbar count={this.props.count} />
+        </Header>
+      </div>
+    );
+  }
 }
 
 export default Home;
